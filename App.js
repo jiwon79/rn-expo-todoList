d1663f0551190d69ed3044d3d46a39efda179387
jiwon79 <19-079@ksa.hs.kr>
2021-08-01 23:10:48 +0900

    feat: Add Todo Item

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './components/Header';
+import TodoItem from './components/TodoItem';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <Header/>
-    </View>
+      <TodoItem title="리액트 네이티브 앱 만들기" done={true}/>
+      <TodoItem title="스터디 진행하기" done={false}/>
+      <TodoItem title="test test test test test test test test test test test test test test test test " done={false}/>
+    </SafeAreaView>
   );
 }
 
---
+++ components/TodoItem.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+
+const TodoItem = ({title, done}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.todo}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          // style = {[styles.done, styles.check_icon]}
+          style={[done ? styles.done: styles.check, styles.check_icon]}
+        >
+          <FontAwesome name="check" color={done ? '#ffffff':'#e0e0e0'} size={14}/>
+        </TouchableOpacity>
+        <Text style={styles.title}>
+          {title}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: '#fff'
+  },
+
+  todo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e5e5'
+  },
+
+  title: {
+    fontSize: 16
+  },
+
+  check_icon: {
+    width: 28,
+    height: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+    borderRadius: 14
+  },
+
+  check: {
+    borderWidth: 1,
+    borderColor: '#e5e5e5',
+  },
+  
+  done: {
+    backgroundColor: '#6830CF',
+  }
+})
+export default TodoItem
